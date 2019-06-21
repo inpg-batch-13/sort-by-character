2451cf46c13114255686f7ba85e584e4b6644d9e
felishaww <felishamiranda1510@gmail.com>
2019-06-22 01:39:14 +0700

    a little fixing

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 function sortByCharacter (text) {
 // #1
 // with built in function:
-
 //return text.split('').sort().join('')
 
 // #2
-//without using .split():
+//without using .sort():
 var arr= text.split('')
-var result = []
   for (var i = 0; i < arr.length; i++){
+
     for (var n = 0; n < arr.length; n++){
+
       if(arr[i] < arr[n]){
         var kosong = arr[i]
         arr[i] = arr[n]
         arr[n] = kosong
       }
+
     }
+
   }
+
 return arr.join('')
 }
 
