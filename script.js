c201e498345b6f739c466646631fa29a9066ca90
rahmatrhd <kickyou44@gmail.com>
2019-06-21 20:55:58 +0700

    solve sort by character

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+function swap(a, b) {
+  var c = a
+  a = b
+  b = c
+}
+
 function sortByCharacter (text) {
   // Your code here
+
+ var str = text.split('')
+  for (var i = 0; i < str.length - 2; i++) {
+    for (var j = 0; j < str.length - i; j++) {
+      if (str[j] > str[j + 1]) {
+        var temp = str[j]
+        str[j] = str[j + 1]
+        str[j + 1] = temp
+      }
+    }
+  }
+
+  return str.join('')
 }
 
 // TEST CASES
