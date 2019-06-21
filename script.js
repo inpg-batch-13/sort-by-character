def8007180acbe138b7681f9b6b2a6c9040b14f5
rahmatrhd <kickyou44@gmail.com>
2019-06-21 14:16:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function sortByCharacter (text) {
+  // Your code here
+}
+
+// TEST CASES
+console.log(sortByCharacter('hello')); // 'ehllo'
+console.log(sortByCharacter('truncate')); // 'acenrttu'
+console.log(sortByCharacter('developer')); // 'deeeloprv'
+console.log(sortByCharacter('software')); // 'aeforstw'
+console.log(sortByCharacter('aegis')); // 'aegis'
