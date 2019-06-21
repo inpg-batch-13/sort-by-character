1836414404b6fb2d309857e3e3acda07e3504454
MuhammadFahmiRidwan <mfahmiridwan50@gmail.com>
2019-06-21 21:12:48 +0700

    done

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function sortByCharacter (text) {
   // Your code here
+  var arr = text.split('')
+  var result = []
+  
+  for(var i = 0; i < arr.length; i++ ){
+    for(var j = 0; j < arr.length; j ++){
+     
+    if(arr[i] < arr[j]){
+      var s = arr[i]
+      arr[i] = arr[j]
+      arr[j] = s
+  }
+}
+}
+    
+  return arr.join('')
 }
 
 // TEST CASES
